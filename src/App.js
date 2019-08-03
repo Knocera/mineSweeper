8e9b7c31a2f573a7116fd3f1a47650816d1d34bd
Keith Nocera <knocera@gmail.com>
2019-08-03 11:25:39 -0400

    created react app, cleaned up boilerplate code and began crafting game state and cell classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class GameState {
+  constructor(){
+    this.playing = 0;
+    this.won = 1
+    this.lost = 2
+  }
 }
+
+class Cell {
+  constructor(){
+    this.isShowing = false
+    this.isBomb = false
+    this.isFlagged = false
+    this.isTouching = 0
+  }
+}
+
 
 export default App;
